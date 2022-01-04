ea2169e65a0ce8d8fcc4fcdbfa68f130186e5db8
Daniel Grieco <daniel_grieco@programmer.net>
2022-01-04 17:40:47 +0100

    Add header and source file for the Advisorbot class

---
+++ Advisorbot.cpp
@@ -0,0 +1,10 @@
+//
+//  Advisorbot.cpp
+//  advisorbot
+//
+//  Created by Daniel Grieco on 1/4/22.
+//
+
+#include "Advisorbot.hpp"
+
+AdvisorBot::AdvisorBot() {}
---
+++ Advisorbot.hpp
@@ -0,0 +1,19 @@
+//
+//  Advisorbot.hpp
+//  advisorbot
+//
+//  Created by Daniel Grieco on 1/4/22.
+//
+
+#ifndef Advisorbot_hpp
+#define Advisorbot_hpp
+
+class AdvisorBot {
+public:
+    AdvisorBot();
+    
+private:
+    
+};
+
+#endif /* Advisorbot_hpp */
