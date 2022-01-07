8eef2058e53d5725e8288a16f58a4dca1d943d60
Daniel Grieco <daniel_grieco@programmer.net>
2022-01-07 17:25:36 +0100

    Add CSVReader class

---
+++ CSVParser.cpp
@@ -0,0 +1,8 @@
+//
+//  CSVParser.cpp
+//  advisorbot
+//
+//  Created by Daniel Grieco on 1/7/22.
+//
+
+#include "CSVParser.hpp"
---
+++ CSVParser.hpp
@@ -0,0 +1,21 @@
+//
+//  CSVParser.hpp
+//  advisorbot
+//
+//  Created by Daniel Grieco on 1/7/22.
+//
+
+#ifndef CSVParser_hpp
+#define CSVParser_hpp
+
+class CSVReader {
+public:
+    // Static class, no constructor or destructor
+    CSVReader() = delete;
+    ~CSVReader() = delete;
+    
+private:
+    
+};
+
+#endif /* CSVParser_hpp */
