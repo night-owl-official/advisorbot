d7befbc903a89a18a47c6ba1a38c8a614d111022
Daniel Grieco <daniel_grieco@programmer.net>
2022-01-07 20:53:44 +0100

    Implemented CSVParser template functions

--- CSVParser.cpp
+++ CSVParser.cpp
@@ -6,3 +6,28 @@
 //
 
 #include "CSVParser.hpp"
+
+#include <vector>
+
+/*
+ Read CSV file while parsing all its lines into OrderBookEntries.
+ */
+std::vector<OrderBookEntry> CSVParser::readCSV(const std::string& csvFilename) {
+    std::vector<OrderBookEntry> temp{};
+    return temp;
+}
+
+/*
+ Take a csv line and turns it into bits where separator happens.
+ */
+std::vector<std::string> CSVParser::tokenize(const std::string& csvLine, char separator) {
+    std::vector<std::string> temp{};
+    return temp;
+}
+
+/*
+ Turn tokens for one order into an OrderBookEntry.
+ */
+OrderBookEntry CSVParser::tokensToOBE(const std::vector<std::string>& tokens) {
+    return OrderBookEntry{0.0, 0.0, "", "", OrderBookType::unknown};
+}
--- CSVParser.hpp
+++ CSVParser.hpp
@@ -8,14 +8,29 @@
 #ifndef CSVParser_hpp
 #define CSVParser_hpp
 
-class CSVReader {
+#include "OrderBookEntry.hpp"
+
+class CSVParser {
 public:
     // Static class, no constructor or destructor
-    CSVReader() = delete;
-    ~CSVReader() = delete;
+    CSVParser() = delete;
+    ~CSVParser() = delete;
     
-private:
+    /*
+     Read CSV file while parsing all its lines into OrderBookEntries.
+     */
+    static std::vector<OrderBookEntry> readCSV(const std::string& csvFilename);
+
+   private:
+    /*
+     Take a csv line and turns it into bits where separator happens.
+     */
+    static std::vector<std::string> tokenize(const std::string& csvLine, char separator);
     
+    /*
+     Turn tokens for one order into an OrderBookEntry.
+     */
+    static OrderBookEntry tokensToOBE(const std::vector<std::string>& tokens);
 };
 
 #endif /* CSVParser_hpp */
