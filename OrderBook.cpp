46a467f23115e69dd2daab249024402dbd2358a7
Daniel Grieco <daniel_grieco@programmer.net>
2022-01-07 20:27:35 +0100

    Add OrderBook class and define its members

---
+++ OrderBook.cpp
@@ -0,0 +1,63 @@
+//
+//  OrderBook.cpp
+//  advisorbot
+//
+//  Created by Daniel Grieco on 1/7/22.
+//
+
+#include "OrderBook.hpp"
+
+/*
+ Initialize order book from a csv file.
+ */
+OrderBook::OrderBook(const std::string& filename) {
+    
+}
+
+/*
+ Return a vector of all known products in the dataset.
+ */
+std::vector<std::string> OrderBook::getKnownProducts() {
+    std::vector<std::string> temp{};
+    return temp;
+}
+
+/*
+ Return vector of Orders according to the sent filters.
+ */
+std::vector<OrderBookEntry> OrderBook::getOrders(OrderBookType type,
+                                                 const std::string& product,
+                                                 const std::string& timestamp) {
+    std::vector<OrderBookEntry> temp{};
+    return temp;
+}
+
+/*
+ Return the earliest time in the orderbook.
+ */
+std::string OrderBook::getEarliestTime() {
+    return "";
+}
+
+/*
+ Return the next time after the
+ sent time in the orderbook.
+ If there is no next timestamp, wraps around to the start.
+ */
+std::string OrderBook::getNextTime(const std::string& timestamp) {
+    return "";
+}
+
+/*
+ Return the highest price in the order book.
+ */
+double OrderBook::getHighestPrice(const std::vector<OrderBookEntry>& orders) {
+    return 0.0;
+}
+
+/*
+ Return the lowest price in the order book.
+ */
+double OrderBook::getLowestPrice(const std::vector<OrderBookEntry>& orders) {
+    return 0.0;
+}
---
+++ OrderBook.hpp
@@ -0,0 +1,60 @@
+//
+//  OrderBook.hpp
+//  advisorbot
+//
+//  Created by Daniel Grieco on 1/7/22.
+//
+
+#ifndef OrderBook_hpp
+#define OrderBook_hpp
+
+#include <vector>
+
+#include "OrderBookEntry.hpp"
+
+class OrderBook {
+public:
+    /*
+     Initialize order book from a csv file.
+     */
+    OrderBook(const std::string& filename);
+    
+    /*
+     Return a vector of all known products in the dataset.
+     */
+    std::vector<std::string> getKnownProducts();
+    
+    /*
+     Return vector of Orders according to the sent filters.
+     */
+    std::vector<OrderBookEntry> getOrders(OrderBookType type,
+                                          const std::string& product,
+                                          const std::string& timestamp);
+    
+    /*
+     Return the earliest time in the orderbook.
+     */
+    std::string getEarliestTime();
+    
+    /*
+     Return the next time after the
+     sent time in the orderbook.
+     If there is no next timestamp, wraps around to the start.
+     */
+    std::string getNextTime(const std::string& timestamp);
+    
+    /*
+     Return the highest price in the order book.
+     */
+    double getHighestPrice(const std::vector<OrderBookEntry>& orders);
+    
+    /*
+     Return the lowest price in the order book.
+     */
+    double getLowestPrice(const std::vector<OrderBookEntry>& orders);
+
+private:
+    std::vector<OrderBookEntry> m_orders;   // Store all the orderbook entries
+};
+
+#endif /* OrderBook_hpp */
