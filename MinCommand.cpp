d3e09c762ef416982a3347c3f059eb91cb39e736
Daniel Grieco <daniel_grieco@programmer.net>
2022-01-09 17:53:39 +0100

    Implement run function for min command

--- MinCommand.cpp
+++ MinCommand.cpp
@@ -20,7 +20,28 @@ bool MinCommand::run(const OrderBook& orderBook) {
     // Invalid parameters, return false
     if (!validParams)   return validParams;
     
+    // Orderbook with only filtered entries based on the given parameters.
+    std::vector<OrderBookEntry> filteredOrders =
+        orderBook.getOrders(m_orderType, m_product, orderBook.getCurrentTimestamp());
     
+    // The min price in the filtered orderbook.
+    double minPrice = OrderBook::getLowestPrice(filteredOrders);
+    
+    // Convert orderbooktype to string
+    std::string orderTypeString = "";
+    switch (m_orderType) {
+        case OrderBookType::bid:
+            orderTypeString = "bid";
+            break;
+        case OrderBookType::ask:
+            orderTypeString = "ask";
+            break;
+        default:
+            break;
+    }
+    
+    // Display min price.
+    std::cout << "The min " << orderTypeString << " price for " << m_product << " is " << minPrice << std::endl;
     
     return validParams;
 }
