1cdc21f85ea8fdf80d984ebeead4fbdb089dd90a
Daniel Grieco <daniel_grieco@programmer.net>
2022-01-09 16:39:10 +0100

    Implement step command

--- StepCommand.cpp
+++ StepCommand.cpp
@@ -7,18 +7,29 @@
 
 #include "StepCommand.hpp"
 
+#include <iostream>
+
 StepCommand::StepCommand(const std::vector<std::string>& cmdParams) : Command(cmdParams) {}
 
 /*
  Run the command.
  */
 bool StepCommand::run(const OrderBook& orderBook) {
-    return true;
+    bool validParams = areParamsValid();
+    
+    // Invalid parameters, return false.
+    if (!validParams)   return validParams;
+    
+    // Display the next timestamp.
+    std::cout << "Now at " << orderBook.getNextTime(orderBook.getCurrentTimestamp()) << std::endl;
+    
+    return validParams;
 }
 
 /*
  Verify that the parameters are valid for this command.
  */
 bool StepCommand::areParamsValid() {
-    return true;
+    // No parameters allowed for this command.
+    return m_params.size() == 0;
 }
